29e638b1edc9fb7056fa08e7f1f673f69c601819
Rhys Doyle <rhys.doyle@gmail.com>
2018-05-19 14:16:46 +1000

    update 19.05.18

--- script-min.js
+++ script-min.js
@@ -1,6 +1,12 @@
 data.fixtures.forEach((game) => {
     console.log(game);
 
+    //set time to find zone
+    const timeZone = new Date();
+    
+    console.log(timeZone);
+
+
     //set current date and game date in milliseconds
      const todayInMilliseconds = new Date().getTime();
      const gameTimeInMilliseconds = new Date(game.match.startDateTimes[1].date).getTime();
@@ -54,10 +60,12 @@ data.fixtures.forEach((game) => {
 
         const homeTotal = document.createElement('span');
         homeTotal.className = 'totalScore';
+        homeTotal.textContent = `${game.homeTeam.totalScore}`;
         homeScore.appendChild(homeTotal);
 
         const homeBreakdown = document.createElement('span');
         homeBreakdown.className = 'breakdownScore';
+        homeBreakdown.textContent = `${game.homeTeam.goals}.${game.homeTeam.behinds}`;
         homeScore.appendChild(homeBreakdown);
 
         const gameOver = document.createElement('span');
@@ -71,20 +79,71 @@ data.fixtures.forEach((game) => {
 
         const awayTotal = document.createElement('div');
         awayTotal.className = 'totalScore';
+        awayTotal.textContent = `${game.awayTeam.totalScore}`;
         awayScore.appendChild(awayTotal);
 
         const awayBreakdown = document.createElement('div');
         awayBreakdown.className = 'breakdownScore';
+        awayBreakdown.textContent = `${game.awayTeam.goals}.${game.awayTeam.behinds}`;
         awayScore.appendChild(awayBreakdown);
 
         const resultDiv = document.createElement('div');
         resultDiv.className = 'gameResult';
         gameInfoBox.appendChild(resultDiv);
 
-        const result =
+        const result = document.createElement('span');
+        result.className = 'resultInfo';
+        result.textContent = `${game.match.matchResultText}`;
+        resultDiv.appendChild(result);
 
+    } else {
 
+        //creating homePosition span
+        const homePosition = document.createElement('span');
+        homePosition.className = 'position';
+        homePosition.textContent = `${game.homeTeam.ladderPositionText}`;
 
+        //locating homePosition span within gameMetadataBox div
+        gameMetadataBox.appendChild(homePosition);
+    
+        //creating eta h3
+        const makeETA = document.createElement('h3');
+        makeETA.className = 'eta';
+
+        //locating makeETA h3 within gameMetadataBox div
+        gameMetadataBox.appendChild(makeETA);
+
+        //creating awayPosition span
+        const awayPosition = document.createElement('span');
+        awayPosition.className = 'position';
+        awayPosition.textContent = `${game.awayTeam.ladderPositionText}`;
+
+        //locating awayPosition span within gameMetadataBox div
+        gameMetadataBox.appendChild(awayPosition);
+
+        //creating time of game div
+        const timeAndPlaceBox = document.createElement('div');
+        timeAndPlaceBox.className = 'timePlaceBox';
+    
+        //creating time and place span
+        const timeAndPlace = document.createElement('span');
+        timeAndPlace.className = 'timePlace';
+        timeAndPlace.textContent = `${game.match.startDateTimes[1].time} my time, ${game.match.venueShortName}`;
+
+        //creating awayTeam div
+        const awayLogoBox = document.createElement('div');
+        awayLogoBox.className = 'logobox';
+
+        //creating awayTeam img
+        const awayLogo = document.createElement('img');
+        awayLogo.className = 'logo';
+        awayLogo.src = `images/${game.awayTeam.teamName}_logo.jpg`.toLowerCase();
+    
+        //locating awayTeam img within awayTeam div
+        awayLogoBox.appendChild(awayLogo);
+    
+        //locating awayTeam div within gameContainer
+        gameBox.appendChild(awayLogoBox);
 
     }
     
@@ -95,52 +154,7 @@ data.fixtures.forEach((game) => {
 
 
 
-    //creating homePosition span
-    const homePosition = document.createElement('span');
-    homePosition.className = 'position';
-    homePosition.textContent = `${game.homeTeam.ladderPositionText}`;
 
-    //locating homePosition span within gameMetadataBox div
-    gameMetadataBox.appendChild(homePosition);
-    
-    //creating eta h3
-    const makeETA = document.createElement('h3');
-    makeETA.className = 'eta';
-
-    //locating makeETA h3 within gameMetadataBox div
-    gameMetadataBox.appendChild(makeETA);
-
-    //creating awayPosition span
-    const awayPosition = document.createElement('span');
-    awayPosition.className = 'position';
-    awayPosition.textContent = `${game.awayTeam.ladderPositionText}`;
-
-    //locating awayPosition span within gameMetadataBox div
-    gameMetadataBox.appendChild(awayPosition);
-
-    //creating time of game div
-    const timeAndPlaceBox = document.createElement('div');
-    timeAndPlaceBox.className = 'timePlaceBox';
-    
-    //creating time and place span
-    const timeAndPlace = document.createElement('span');
-    timeAndPlace.className = 'timePlace';
-    timeAndPlace.textContent = `${game.match.startDateTimes[1].time} my time, ${game.match.venueShortName}`;
-
-    //creating awayTeam div
-    const awayLogoBox = document.createElement('div');
-    awayLogoBox.className = 'logobox';
-
-    //creating awayTeam img
-    const awayLogo = document.createElement('img');
-    awayLogo.className = 'logo';
-    awayLogo.src = `images/${game.awayTeam.teamName}_logo.jpg`.toLowerCase();
-    
-    //locating awayTeam img within awayTeam div
-    awayLogoBox.appendChild(awayLogo);
-    
-    //locating awayTeam div within gameContainer
-    gameBox.appendChild(awayLogoBox);
 
 
     /*const element = document.createElement('span');
